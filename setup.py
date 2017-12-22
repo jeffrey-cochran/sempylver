e09070e0717cec81e75a290385b87680bf4dc2e0
jeffrey-cochran <koenigcochran@gmail.com>
2017-12-22 13:23:36 -0500

    instantiate main and setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='sempylver',
+      version='0.0.1',
+      description='A simple tool for tracking the semantic version of projects',
+      author='Jeff Cochran',
+      author_email='jeffrey.david.cochran@gmail.com',
+      packages=find_packages(),
+      entry_points={
+        'console_scripts': [
+            'sempylver = sempylver.main:cli'
+        ]
+      }
+)
