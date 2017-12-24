50df2168f88fad5b471a4c1e2d66ea1a5ee9e630
jeffrey-cochran <koenigcochran@gmail.com>
2017-12-24 00:47:44 -0500

    updated sub-command and action

--- sempylver/__main__.py
+++ sempylver/__main__.py
@@ -6,16 +6,24 @@ def main():
     #
     # Define an argument parser for commit-msg.py
     parser = argparse.ArgumentParser(description='Tool for simple semantic versioning.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    subparsers = parser.add_subparsers(help="""Possible actions:
-- config""")
+    subparsers = parser.add_subparsers(
+        help="""Possible actions:
+- config""",
+        dest='command'
+    )
     #
     # Config parser
     parser_config = subparsers.add_parser('config', help='Set values in the global config file')
     parser_config.add_argument('-d', metavar='d', type=str, help='the config option to set')
     #
-    # Print the args
+    # Get args
     args_dict = vars(parser.parse_args())
-    print(args_dict)
+    #
+    # Determine action
+    action = args_dict.pop('action')
+    #
+    # Perform action
+    action(**args_dict)
 
 
 if __name__ == "__main__":
