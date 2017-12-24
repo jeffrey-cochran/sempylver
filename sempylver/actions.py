c8da629d2ba5de77bd369e71f08cbaafebc7f200
jeffrey-cochran <koenigcochran@gmail.com>
2017-12-24 11:08:39 -0500

    moved commit msg etc

--- sempylver/actions.py
+++ sempylver/actions.py
@@ -1,4 +1,5 @@
-from sempylver.utils import config_parser
+from os.path import isdir, join
+from sempylver.utils import config_parser, write_commit_msg_hook
 
 
 def set_config(**kwargs):
@@ -7,6 +8,31 @@ def set_config(**kwargs):
         z.set(key, value)
     z.write()
 
+
+def track_project(project_directory=None):
+    #
+    z = config_parser()
+    try:
+        working_directory = z.config_opts['working_directory']
+    except KeyError:
+        working_directory = None
+    #
+    is_relative_path = False
+    if working_directory is not None:
+        git_hook_directory = join(join(project_directory, '.git'), 'hooks')
+        is_relative_path = isdir(join(working_directory, git_hook_directory))
+    #
+    if is_relative_path:
+        git_hook_directory = join(working_directory, git_hook_directory)
+    elif not isdir(git_hook_directory):
+        raise Exception('The target directory, %s, does not appear to be a git repository.' % project_directory)
+    #
+    write_commit_msg_hook(git_hook_directory)
+    #
+    return
+
+
 actions = {
-    'config': set_config
+    'config': set_config,
+    'track': track_project,
 }
--- sempylver/utils.py
+++ sempylver/utils.py
@@ -1,5 +1,7 @@
+from shutil import copy2
+from os.path import join
 import yaml
-from sempylver.constants import global_config
+from sempylver.constants import global_config, this_dir
 
 
 class config_parser(object):
@@ -16,6 +18,8 @@ class config_parser(object):
         with open(config_file, 'r') as cf:
             config_opts_string = cf.read()
             self.config_opts = yaml.load(config_opts_string)
+        #
+        return
 
     def set(
         self,
@@ -25,8 +29,19 @@ class config_parser(object):
         #
         # Set config dict value
         self.config_opts[config_name] = value
+        #
         return self
 
     def write(self):
         with open(self.config_file, 'w') as cf:
             yaml.dump(self.config_opts, cf, default_flow_style=False)
+        #
+        return
+
+
+def write_commit_msg_hook(git_hook_directory):
+    #
+    copy2(join(this_dir, 'commit-msg'), git_hook_directory)
+    copy2(join(this_dir, 'commit_msg.py'), git_hook_directory)
+    #
+    return
