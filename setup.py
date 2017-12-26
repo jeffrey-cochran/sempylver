23f24d4d45efc80785f83c6514874983a07f6317
jeffrey-cochran <koenigcochran@gmail.com>
2017-12-25 22:38:22 -0500

    confirmed correctly replaces version string

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@ from distutils.core import setup
 from setuptools import find_packages
 
 
+with open('__version__', 'rb') as f:
+    version = f.read().strip()
+
+
 setup(
+
     name='sempylver',
-    version='0.0.1',
+    version=version,
     description='A simple tool for tracking the semantic version of projects',
     author='Jeff Cochran',
     author_email='jeffrey.david.cochran@gmail.com',
